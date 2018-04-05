e471682b69aa96c3eb3abfb50d075e6177d3e713
mskian <veerfun23@gmail.com>
2018-04-05 19:55:01 +0530

    enhancement

--- push.php
+++ push.php
@@ -12,6 +12,11 @@ $title= $_POST["title"];
 $message= $_POST["message"];
 $postlink= $_POST["postlink"];
 
+$token = htmlspecialchars($token,ENT_COMPAT);
+$title = htmlspecialchars($title,ENT_COMPAT);
+$message = htmlspecialchars($message,ENT_COMPAT);
+$postlink = htmlspecialchars($postlink,ENT_COMPAT);
+
 // Push Data's
 $data = array(
 "to" => "$token",
@@ -55,68 +60,62 @@ curl_close ($ch);
 <html lang="en">
 <head>
 
-<!-- meta character set -->
 <meta charset="utf-8">
-
-<!-- Always force latest IE rendering engine or request Chrome Frame -->
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-<!-- Mobile Specific Meta -->
-<meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <title>FCM Push Notification</title>
 <meta name="Description" content="Subscribe to our Blog Post Updates.">
-<link href='https://www.mskian.com/msk.ico' rel='icon' type='image/x-icon'/>
 
 <!-- CSS and Fonts -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
 
 <style>
 body {
   background: #eee !important;
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  
+  font-family: 'Exo 2', sans-serif;
 }
 .login-form{
-	margin:3% auto 0;
-	max-width:380px;
+  margin:3% auto 0;
+  max-width:380px;
 }
 .login-form h1{
-	font-size: 30pt;
-	font-weight: 700;
+  font-size: 30pt;
+  font-weight: 700;
     letter-spacing: -1px;
-    font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    font-family: 'Exo 2', sans-serif;
 }
 .form-header,.form-footer{
-	background-color: rgba(255, 255, 255, .8);
-  	border: 1px solid rgba(0,0,0,0.1);
+  background-color: rgba(255, 255, 255, .8);
+    border: 1px solid rgba(0,0,0,0.1);
 }
 .form-signin{
-	padding: 45px 35px 45px;
-  	background-color: #fff;
-  	border: 1px solid rgba(0,0,0,0.1);  
-  	border-bottom: 0px; 
-  	border-top: 0px;  
+  padding: 45px 35px 45px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.1);  
+    border-bottom: 0px; 
+    border-top: 0px;  
 }
 .form-register{
-	padding: 45px 35px 45px;
-  	background-color: #fff;
-  	border: 1px solid rgba(0,0,0,0.1);  
-  	border-bottom: 0px; 
-  	border-top: 0px; 
+  padding: 45px 35px 45px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.1);  
+    border-bottom: 0px; 
+    border-top: 0px; 
 }
 .form-header{
-	text-align: center;
-	padding: 15px 40px;
-	border-radius: 10px 10px 0 0;
+  text-align: center;
+  padding: 15px 40px;
+  border-radius: 10px 10px 0 0;
 }
 .form-header i{font-size:60px;}
 .form-footer {
-	padding: 15px 40px;	
+  padding: 15px 40px; 
 }
 .form-signin-heading{
-	margin-bottom: 30px;
+  margin-bottom: 30px;
 }
 .bt-login{
     background-color: #ff8627;
@@ -168,17 +167,34 @@ textarea.parsley-error:focus {
     opacity: 1;
     color: #a94442;
 }
+pre {
+  background-color: #f7f7f9;
+  border: 1px solid #e1e1e8;
+color: #336cac;
+font-size: 17px;
+}
+pre:before {
+    color: #333;
+}
+.btn {
+    margin-bottom:4px;white-space: normal;
+}
+.input {
+    margin-bottom:4px;white-space: normal;
+}
 </style>
 
 
-<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
 </head>
 <body>
@@ -187,21 +203,22 @@ textarea.parsley-error:focus {
 <div class="container">
 <div class="login-form">
 <h2 class="text-center">Send Push Notification</h2>
+<br>
 <div class="form-header">
-<i class="fa fa-bell"></i>
+<i class="fas fa-bell"></i>
 </div>
 <form class="form-signin" method="post">
 <div class="form-group">
-<textarea class="form-control" name="token" required data-parsley-trigger="keyup" placeholder="FCM Token"></textarea>
+<textarea class="form-control" name="token" placeholder="FCM Token" data-parsley-required="true" data-parsley-error-message="Please Enter the Token"></textarea>
 </div>
 <div class="form-group">
-<input class="form-control" type="text" name="title" required data-parsley-trigger="keyup" placeholder="Push Title">
+<input class="form-control" type="text" name="title" placeholder="Push Title" data-parsley-required="true" data-parsley-error-message="Enter the Title">
 </div>
 <div class="form-group">
-<textarea class="form-control" name="message" required data-parsley-trigger="keyup" placeholder="Push Message"></textarea>
+<textarea class="form-control" name="message" placeholder="Push Message" data-parsley-required="true" data-parsley-error-message="Enter a Push Message"></textarea>
 </div>
 <div class="form-group">
-<input class="form-control" type="text" name="postlink" required data-parsley-trigger="keyup" placeholder="Click Action Link">
+<input class="form-control" type="text" name="postlink" placeholder="Click Action Link" data-parsley-required="true" data-parsley-error-message="Enter a Link">
 </div>
 <button type="submit" class="btn btn-block bt-login">Send Push</button>
 </form>
@@ -212,7 +229,7 @@ textarea.parsley-error:focus {
 
 <div class="container">
 <div class="row">
-<div class="col-lg-6 col-lg-offset-3 text-center">
+<div class="col-lg-6 col-lg-offset-3 mx-auto">
 <?php
 if(isset($_POST['token'])) {
 // Display Output
@@ -227,15 +244,14 @@ echo "<pre>The Json Data : $data_string</pre>";
 <br />
 <br />
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.5.1/parsley.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.8.1/parsley.min.js" integrity="sha256-XqEmjxbIPXDk11mQpk9cpZxYT+8mRyVIkko8mQzX3y8=" crossorigin="anonymous"></script>
 
 <script>
 $(document).ready(function(){
-	$('form').parsley();
+  $('form').parsley();
 });
 </script>
-
 
 </body>
 </html>
